70b412052362a5f7343eeb52103e1ff551f0c483
G. Willson <greg@biomassiv.es>
2017-01-07 19:16:33 -0700

    Update config.rb

--- config.rb
+++ config.rb
@@ -1,5 +1,5 @@
 # If you change, Keep the structure with the dot. [0-9 a-f]
-$token = "56225f.9096af3559800a6a"
+$token = "56225f.8429ki7432755b9g"
 # Total memory of master
 $master_memory = 1536
 # Increment to have more nodes
@@ -7,4 +7,4 @@ $worker_count = 2
 # Total memory of nodes
 $worker_memory = 1536
 # Add Grafana with InfluxDB (work with heapster)
-$grafana = false
+$grafana = true
